825e543b75d6c0cbd996541693b4b5acbc2a6d69
adria <adria@REX54X>
2026-03-19 13:34:24 -0600

    fin 1

---
+++ 1_Validar_edad/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+int edad;
+bool valido;
+do
+{
+    Console.Clear();
+    Console.Write("Ingrese su edad:");
+    valido = int.TryParse(Console.ReadLine(), out edad);
+    if (!valido || edad < 0 || edad > 120)
+    {
+        valido = false;
+        Console.WriteLine("Edad No valida!");
+        Thread.Sleep(500);
+    }
+
+} while (!valido);
+Console.WriteLine("Edad valida");
+Console.ReadKey();
